0c4f3bc65c030dba53c6e7689eaff9a98ce9e63e
shaunlsy <shaunlowshuennyuh@hotmail.com>
2020-01-21 14:09:15 +0000

    set max temp according based on power saving mode

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -10,18 +10,34 @@ describe("Thermostat", function() {
   });
 
   it("temperature can increase with up function", function() {
-    thermostat.up(5)
+    thermostat.up(5);
     expect(thermostat.temp).toEqual(25);
   });
 
   it("temperature can decrease with down function", function() {
-    thermostat.down(10)
+    thermostat.down(10);
     expect(thermostat.temp).toEqual(10);
   });
 
   it("temperature can increase and decrease", function() {
-    thermostat.up(10)
-    thermostat.down(5)
+    thermostat.up(10);
+    thermostat.down(5);
     expect(thermostat.temp).toEqual(25);
+  });
+
+  it("has a minimum temperature of 10", function() {
+    expect(thermostat.minTemp).toEqual(10);
+  });
+
+  describe("Power saving mode", function() {
+    it("has maximum temperature of 25 when the mode is on", function() {
+      thermostat.powerSavingMode("On");
+      expect(thermostat.maxTemp).toEqual(25);
+    });
+
+    it("has maximum temperature of 32 when the mode is off", function() {
+      thermostat.powerSavingMode("Off");
+      expect(thermostat.maxTemp).toEqual(32);
+    });
   });
 });
--- src/Thermostat.js
+++ src/Thermostat.js
@@ -1,11 +1,27 @@
+const MINTEMP = 10;
+
 function Thermostat() {
-  this.temp = 20
+  this.temp = 20;
+  this.minTemp = MINTEMP;
+  this.maxTemp;
 }
 
 Thermostat.prototype.up = function(degrees) {
-  this.temp += degrees
+  this.temp += degrees;
 };
 
 Thermostat.prototype.down = function(degrees) {
-  this.temp -= degrees
+  this.temp -= degrees;
+};
+
+Thermostat.prototype.powerSavingMode = function(status) {
+  if (status === "On") {
+    return this._setMaxTemp(25);
+  } else if (status === "Off") {
+    return this._setMaxTemp(32);
+  }
+};
+
+Thermostat.prototype._setMaxTemp = function(degrees) {
+  this.maxTemp = degrees;
 };
